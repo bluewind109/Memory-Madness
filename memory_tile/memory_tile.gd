906eab5e8e5bcbfd21a20ebaa5ae5f635892a7f2
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2024-04-19 16:50:42 +0700

    Reveal tile on selected

--- memory_tile/memory_tile.gd
+++ memory_tile/memory_tile.gd
@@ -4,12 +4,18 @@ extends TextureButton
 @onready var item_image = $ItemImage
 
 var _item_name: String
+var _can_select: bool = true
 
 func _ready():
-	pass
+	SignalManager.on_selection_disabled.connect(on_selection_disabled)
+	SignalManager.on_selection_enabled.connect(on_selection_enabled)
 	
 func get_item_name() -> String:
 	return _item_name
+	
+func reveal(val: bool) -> void:
+	frame_image.visible = val
+	item_image.visible = val
 	
 func setup(
 	ii_dict: Dictionary, 
@@ -18,3 +24,14 @@ func setup(
 	frame_image.texture = _frame_img
 	item_image.texture = ii_dict.item_texture
 	_item_name = ii_dict.item_name
+	reveal(false)
+
+func on_selection_disabled() -> void:
+	_can_select = false
+	
+func on_selection_enabled() -> void:
+	_can_select = true
+
+func _on_pressed():
+	if (_can_select == true):
+		reveal(true)
