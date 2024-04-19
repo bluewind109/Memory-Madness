688c31e87a4bf034267c721bf3a6b768d85d409f
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2024-04-19 16:20:44 +0700

    Add level selection

--- game_screen/game_screen.gd
+++ game_screen/game_screen.gd
@@ -1,8 +1,16 @@
 extends Control
 
+@onready var exit_sound = $ExitSound
 
 func _ready():
+	SignalManager.on_level_selected.connect(on_level_selected)
+	
+func on_level_selected(level_num: int) -> void:
+	var level_selection = GameManager.get_level_selection(level_num)
+	var frame_image = ImageManager.get_random_frame_image()
+	
 	pass
 	
 func _on_exit_button_pressed() -> void:
+	SoundManager.play_button_click(exit_sound)
 	SignalManager.on_game_exit_pressed.emit()
